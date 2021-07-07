b90457a44ba737b95cff11f29200f6d37e191e1a
Deo Valiandro. M <mdv16h@student.unhas.ac.id>
2021-07-07 23:13:47 +0800

    update fix key builder and encryption

--- test.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import DES
-
-mode = DES.MODE_ECB
-cipher = DES.new("HELLODEO", mode)
-result = cipher.encrypt("DEOVALIA")
-inhex = result.hex()
-# print(bin(int(inhex, 16))[2:])
-print(inhex)
