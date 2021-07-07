7c5b0a1abf47788281585e8ca80e68c2683475fa
Deo Valiandro. M <mdv16h@student.unhas.ac.id>
2021-07-07 15:49:41 +0800

    refactor to standart code

--- des.py
+++ des.py
@@ -1,6 +1,6 @@
 # left shift rotate
-def rotate(l, n):
-    return l[n:] + l[:n]
+def rotate(data, n):
+    return data[n:] + data[:n]
 
 
 # key generate
@@ -12,32 +12,32 @@ for i in range(len(externalKey)):
     bits = '00000000'[len(bits):] + bits
     result.extend([int(b) for b in bits])
 
-PC1 = [ 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
-        10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
-        63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
-        14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
+PC1 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
+       10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+       63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
+       14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
 
-PC2 = [ 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
-        23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
-        41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-        44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+PC2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+       23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
+       41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+       44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
 
-round = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+keyRound = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
-nresult = []
+rawResult = []
 Key = []
 
 for i in range(len(PC1)):
-    nresult.append(result[PC1[i]-1])
+    rawResult.append(result[PC1[i] - 1])
 
-C, D = nresult[:23], nresult[23:]
+C, D = rawResult[:23], rawResult[23:]
 
-for i in range(len(round)):
-    C, D = rotate(C, round[i]), rotate(D, round[i])
+for i in range(len(keyRound)):
+    C, D = rotate(C, keyRound[i]), rotate(D, keyRound[i])
     Ki = C + D
     aur = []
     for j in range(len(PC2)):
-        aur.append(Ki[PC2[j]-1])
+        aur.append(Ki[PC2[j] - 1])
     Key.append(aur)
 
 for i in range(len(Key)):
